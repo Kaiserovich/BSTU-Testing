0d801dc732fd2aa20cbe8ec6e30c02a8b6bc744f
Процукович Кирилл <31707173+Kaiserovich@users.noreply.github.com>
2020-01-14 11:43:31 +0300

    F1

---
+++ L8_F1/GitHubAutomation/Tests/ShopTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Framework.Driver;
+
+using Framework.Services;
+using GitHubAutomation.Tests;
+using GitHubAutomation.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.Extensions;
+
+namespace Framework.Tests
+{
+    class ShopTests : TestConfig
+    {
+       
+        [Test]
+        public void CheckDailyNew()
+        {
+            MakeScreenshotWhenFail(() =>
+            {
+                Driver.Navigate().GoToUrl("https://www.romwe.com/");
+                NewPage sPage = new MainPage(Driver).ClickOnNewField();
+                Assert.AreEqual("Daily New", sPage.pageName.Text.Substring(0, 9));
+
+            });
+        }
+
+    }
+}
